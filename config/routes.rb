8955631141e8e2323eafeab442784ca7b7b3b2e2
Eduardo Straub <eduardo.straub1@gmail.com>
2019-06-24 19:26:25 -0400

    Se avanza en la actividad

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
-  resources :histories
+  get 'users/new'
 
+  get 'users/create'
+
+  resources :histories
+  resources :sessions, only: [:create, :destroy]
   root "histories#index"
+  get 'users/sign_up', to: "users#new"
+  post 'users', to: "users#create"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
